42ef0de9e9e4d929d8b58bdf3aad6859989b8a33
Arpit Mohan <arpit@appsmith.com>
2021-02-18 21:55:03 +0530

    Yet another commit

--- app/client/build.sh
+++ app/client/build.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-GIT_SHA=$(eval git rev-parse HEAD)
-echo $GIT_SHA
+echo "REACT_APP_SENTRY_RELEASE: $REACT_APP_SENTRY_RELEASE"
 echo "Sentry Auth Token: $SENTRY_AUTH_TOKEN"
 
-REACT_APP_SENTRY_RELEASE=$GIT_SHA EXTEND_ESLINT=true craco --max-old-space-size=4096 build --config craco.build.config.js
+EXTEND_ESLINT=true craco --max-old-space-size=4096 build --config craco.build.config.js
 
 rm ./build/static/js/*.js.map
 echo "build finished"
